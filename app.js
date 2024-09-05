3d4933bc978bff1388e1274f1ddbf476e526e3f7
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-09-05 11:43:19 -0300

    Implementando a funcionalidade de busca

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
- console.log(dados)
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa"); // Obtém a seção onde os resultados serão exibidos
+
+    let resultados = ""; // Inicializa uma string vazia para armazenar os resultados da pesquisa
+
+    // Itera sobre cada elemento do array 'dados' e cria um elemento HTML para cada resultado
+    for (let dado of dados) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a> </h2>
+                <p class="descricao-meta">${dado.descricao}</p> <a href=${dado.link} target="_blank">Mais informações</a> </div>
+        `;
+    }
+
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+}
